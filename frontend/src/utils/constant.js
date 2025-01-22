cb18a036a8f01da01a75c57894a80785640a31ff
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-23 01:29:06 +0530

    final

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,6 @@
-export const BASE_URL = process.env.NODE_ENV === "production"
-  ? "http://localhost:8000" // Update this to your backend production URL
-  : "http://localhost:8000";
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+
+export const USER_API_END_POINT = "https://jobportalmern-back3.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobportalmern-back3.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobportalmern-back3.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobportalmern-back3.onrender.com/api/v1/company";
