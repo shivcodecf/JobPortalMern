f019f10ecd41d683204dcf52ff3a4fd54cdd419b
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-23 01:19:45 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,20 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js"
-import path from "path";
+
 
 dotenv.config();
 
 const app = express();
-const _dirname = path.resolve();
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+
 
 const corsOptions = {
     origin: "http://localhost:5173", // Correct URL
@@ -42,11 +45,9 @@ app.use("/api/v1/job", jobRoute);
 
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
 
-app.get('*',(_,res)=>{
- res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
-})
+
+
 
 
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const BASE_URL = process.env.NODE_ENV === "production"
+  ? "http://localhost:8000" // Update this to your backend production URL
+  : "http://localhost:8000";
 
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
